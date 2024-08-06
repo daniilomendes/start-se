af823807cb86b2b2f442dfcc2ad970c637284d2c
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-06 19:43:11 -0300

    feat: add signin app

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == "GET":
@@ -31,4 +32,20 @@ def cadastro(request):
             password=senha
         )
 
+        return redirect('/usuarios/logar')
+
+def logar(request):
+    if request.method == "GET":
+        return render(request, 'logar.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/empresarios/cadastrar_empresa')
+        
+        messages.add_message(request, constants.ERROR, "Usuário ou senha inválidos.")
         return redirect('/usuarios/logar')
