adfa946566fc7e7fc37d57e0d938558190e9aafc
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-06 19:18:59 -0300

    feat: add signup app

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
-# Create your views here.
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'A senhas não coincidem.')
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'A senha precisar ter pelo menos 6 dígitos.')
+            return redirect('/usuarios/cadastro')
+        
+        users = User.objects.filter(username=username)
+
+        if users.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse nome.')
+            return redirect('/usuarios/cadastro')
+
+        user = User.objects.create_user(
+            username=username,
+            password=senha
+        )
+
+        return redirect('/usuarios/logar')
