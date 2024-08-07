1e6d4c660a99615e52e2534d9aa7aa18c9f7ae04
Danilo Mendes <danilo.mirandha@gmail.com>
2024-08-07 10:44:38 -0300

    feat: add metrics registry

--- empresarios/urls.py
+++ empresarios/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('empresa/<int:id>', views.empresa, name="empresa"),
     path('add_doc/<int:id>', views.add_doc, name="add_doc"),
     path('excluir_dc/<int:id>', views.excluir_dc, name="excluir_dc"),
+    path('add_metrica/<int:id>', views.add_metrica, name="add_metrica"),
 ]
--- empresarios/views.py
+++ empresarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Empresas, Documento
+from .models import Empresas, Documento, Metricas
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -101,7 +101,7 @@ def add_doc(request, id):
 
 def excluir_dc(request, id):
     documento = Documento.objects.get(id=id)
-    
+
     if documento.empresa.user != request.user:
         messages.add_message(request, constants.ERROR, "Ação não permitida.")
         return redirect(f'/empresarios/empresa/{empresa.id}')
@@ -109,3 +109,19 @@ def excluir_dc(request, id):
     documento.delete()
     messages.add_message(request, constants.SUCCESS, "Arquivo deletado com sucesso.")
     return redirect(f'/empresarios/empresa/{documento.empresa.id}')
+
+def add_metrica(request, id):
+    empresa = Empresas.objects.get(id=id)
+    titulo = request.POST.get("titulo")
+    valor = request.POST.get("valor")
+
+    metrica = Metricas(
+        empresa=empresa,
+        titulo=titulo,
+        valor=valor
+    )
+
+    metrica.save()
+
+    messages.add_message(request, constants.SUCCESS, "Métrica cadastrada com sucesso.")
+    return redirect(f'/empresarios/empresa/{empresa.id}')
